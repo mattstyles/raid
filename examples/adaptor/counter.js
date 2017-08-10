95f0960e461fb6d6d901df04c024e6deab524008
Matt Styles <matt@personalurban.com>
2017-08-10 18:23:52 +0100

    update adaptor example to common example components

--- examples/adaptor/counter.js
+++ examples/adaptor/counter.js
@@ -1,34 +1,15 @@
 
 import {createSelector} from 'reselect'
 
-import Button from '../_common/actionButton'
+import {Button, theme} from '../_common'
 import {connect} from './store'
 import {ACTIONS, dispatch} from './actions'
 
-const styles = {
-  counter: {
-    display: 'inline-block',
-    padding: '8px 0px 8px 8px',
-    background: 'rgb(255, 255, 255)',
-    border: '1px solid rgb(230,232,238)',
-    borderRadius: '3px'
-  },
-  count: {
-    display: 'inline-block',
-    fontSize: '28px',
-    margin: '0px 16px 0px 8px',
-    verticalAlign: 'middle'
-  },
-  counterControls: {
-    display: 'inline-block'
-  }
-}
-
 const Counter = ({count}) => {
   return (
-    <div style={styles.counter}>
-      <span style={styles.count}>{count}</span>
-      <div style={styles.counterControls}>
+    <div className='counter'>
+      <span className='count'>{count}</span>
+      <div className='controls'>
         <Button
           onClick={dispatch(ACTIONS.ADD)}
         >+</Button>
@@ -36,6 +17,28 @@ const Counter = ({count}) => {
           onClick={dispatch(ACTIONS.SUBTRACT)}
         >-</Button>
       </div>
+      <style jsx>{`
+        .counter {
+          display: inline-block;
+          /*padding: 8px 0px 8px 8px;*/
+          padding: ${theme.basePadding * 0.75}rem;
+          background: ${theme.color.white};
+          border: 1px solid rgb(230, 232, 238);
+          border-radius: ${theme.borderRadius}px;
+        }
+        .count {
+          display: inline-block;
+          font-size: ${theme.baseFontSize * 2.2}rem;
+          margin: 0px ${theme.basePadding}rem 0px ${theme.basePadding * 0.5}rem;
+          vertical-align: middle;
+          min-width: 6rem;
+          text-align: right;
+        }
+        .controls {
+          display: inline-block;
+          vertical-align: middle;
+        }
+      `}</style>
     </div>
   )
 }
--- examples/adaptor/index.js
+++ examples/adaptor/index.js
@@ -1,20 +1,26 @@
 
 import {render} from 'react-dom'
 
-import element from '../_common/element'
+import {element, App} from '../_common'
 import {signal} from './store'
 import {counter} from './update'
 
 import Message from './message'
 import Counter from './counter'
 
-const App = state => (
-  <div>
-    <Message msg='Hello' />
-    <Counter />
-  </div>
-)
+// const App = state => (
+//   <div>
+//     <Message msg='Hello' />
+//     <Counter />
+//   </div>
+// )
 
 signal.register(counter)
 
-signal.observe(state => render(<App />, element))
+signal.observe(state => render(
+  <App state={state}>
+    <Message msg='Hello' />
+    <Counter />
+  </App>,
+  element
+), err => console.error(err))
