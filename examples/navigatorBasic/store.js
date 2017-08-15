370ac43a8c636db4df3ee7263cd9424b0f3f7d76
Matt Styles <matt@personalurban.com>
2017-08-15 17:46:38 +0100

    remove yarn

--- examples/navigatorBasic/store.js
+++ examples/navigatorBasic/store.js
@@ -1,6 +1,6 @@
 
 import {Signal} from 'raid/src'
-import {initial} from 'raid-navigator/src'
+import {initial} from 'raid-navigator'
 import {adaptor} from 'raid-addons/src'
 
 export const signal = new Signal({
