55acdea3a34e463dd78b7433f86ced74c082a6e1
Matt Styles <matt@personalurban.com>
2020-05-26 23:22:08 +0100

    remove unnecessary example common elements

--- packages/examples/examples/_common/index.js
+++ packages/examples/examples/_common/index.js
@@ -1,23 +1,3 @@
 
 export { default as element } from './element'
-export { default as Button } from './actionButton'
-export { default as theme } from './theme'
-export {
-  View,
-  Box,
-  Card,
-  Inline,
-  Main,
-  Code,
-  App
-} from './layout'
-export {
-  Counter,
-  Count
-} from './counter'
-export {
-  H1,
-  H2,
-  H3,
-  P
-} from './text'
+export { App } from './layout'
--- packages/examples/examples/_common/layout.js
+++ packages/examples/examples/_common/layout.js
@@ -1,85 +1,43 @@
 
-import theme from './theme'
-import { GlobalStyle } from '@raid/basic-kit'
 import styled, { ThemeProvider } from 'styled-components'
+import { css } from '@styled-system/css'
+import { GlobalStyle, theme, Pane, Screen, Pre } from '@raid/basic-kit'
 
-export const View = styled('div')`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  min-height: 100vh;
-  -webkit-font-smoothing: antialiased;
-  padding: ${props => props.isPadded && props.theme.basePadding + 'rem'};
-`
+export const Main = styled(Pane)(
+  css({
+    overflow: 'scroll',
+    p: 4,
+    bg: 'background.75'
+  })
+)
 
-export const Box = styled('div')`
-  display: block;
-  padding: ${props => props.isPadded && props.theme.basePadding + 'rem'};
-  margin: ${props => props.isMargin && props.theme.basePadding + 'rem'};
-`
-
-export const Card = styled('div')`
-  display: block;
-  background: ${props => props.theme.color.white};
-  border-radius: ${props => props.theme.borderRadius}px;
-  padding: ${props => props.theme.basePadding}rem;
-`
-
-export const Main = styled('div')`
-  position: relative;
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  padding: ${props => props.theme.basePadding}rem;
-  box-sizing: border-box;
-  overflow: scroll;
-  background: ${props => props.theme.colors.background[75]};
-`
-export const Code = styled('pre')`
-  flex: 1;
-  font-family: 'Source Code Pro', consolas, monospace;
-  font-size: ${props => props.theme.baseFontSize}rem;
-  -webkit-font-smoothing: subpixel-antialiased;
-  margin: 0;
-  padding: ${props => props.theme.basePadding}em;
-  background: ${props => props.theme.color.codeBg};
-  color: ${props => props.theme.color.white};
-  border-left: 1px solid ${props => props.theme.color.border};
-  overflow: scroll;
-  height: 100vh;
-  box-sizing: border-box;
-  font-size: 10px;
-  line-height: 14px;
-`
-
-// export const GlobalStyle = createGlobalStyle`
-//   html {
-//     font-size: 10px;
-//   }
-//   body {
-//     margin: 0;
-//     background: ${props => props.theme.color.bg};
-//     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
-//     color: ${props => props.theme.color.font};
-//   }
-// `
-
-export const Inline = styled('div')`
-  display: inline-block;
-`
+export const StateBlock = styled(Pre)(
+  css({
+    fontSize: 'xs',
+    bg: 'background.800',
+    color: 'white',
+    m: 0,
+    p: 0,
+    borderRadius: 0,
+    overflow: 'scroll',
+    flex: 1
+  })
+)
 
 export const App = ({ state, children }) => {
   return (
     <ThemeProvider theme={theme}>
-      <View>
+      <Screen sx={{ flexDirection: 'row', display: 'flex' }}>
         <GlobalStyle />
         <Main>
           {children}
         </Main>
-        <Code>
-          {JSON.stringify(state, null, '  ')}
-        </Code>
-      </View>
+        <Pane sx={{ p: 4, bg: 'background.800' }}>
+          <StateBlock inset>
+            {JSON.stringify(state, null, '  ')}
+          </StateBlock>
+        </Pane>
+      </Screen>
     </ThemeProvider>
   )
 }
