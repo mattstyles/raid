63b4e46f19fc4ad73cc7e7a364c79fd70bc23c8c
Matt Styles <matt@personalurban.com>
2024-06-18 11:23:16 +0100

    update types for option::fromNullable

--- packages/@raid/option/src/ctor.ts
+++ packages/@raid/option/src/ctor.ts
@@ -1,42 +1,21 @@
+import type { Option } from './model'
 import { None, Some, none, option, some } from './model'
 import type { NonNullish } from './types'
 
-// export function some<T>(value: NonNullable<T>) {
-//   return Some.of(value)
-// }
-
-// export function none<T = never>() {
-//   return None.of<T>()
-// }
-
-function isNonNullish<T>(x: T): x is NonNullable<T> {
-  return x != null
-}
-
 export function fromNullable<
   // biome-ignore lint/suspicious/noExplicitAny: required for TS casting
-  Fn extends (...args: ReadonlyArray<any>) => unknown,
+  Fn extends (...args: ReadonlyArray<any>) => any,
 >(fn: Fn) {
   return (...args: Parameters<Fn>) => {
     const value = fn(...args)
-    // if (value == null) {
-    // if (isNonNullish(value)) {
-    //   // return option(null)
-    //   return some(value)
-    //   // return none()
-    // }
-    // // return option<ReturnType<Fn>>(fn(...args) as ReturnType<Fn>)
-    // // return option(value as ReturnType<Fn>)
-    // // return some(value)
-    // return none()
-    return option(value)
+    return option<ReturnType<Fn>>(value)
   }
 }
 
 // type T = number | null
 // type S = NonNullish<T>
 
-// const inverse = (x: number) => {
+// const inverse = (x: number): number | null => {
 //   if (x === 0) {
 //     return null
 //   }
@@ -47,3 +26,4 @@ export function fromNullable<
 // const oo = fn(12)
 // const o2 = option(12).flatMap(fn)
 // const o3 = option(inverse(0))
+// const fn = fromNullable(inverse)
